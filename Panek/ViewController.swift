2b156effdc42d5c63338478a53ffb34014dcb399
Meph1k <k9nneo4@gmail.com>
2019-05-27 18:37:31 +0200

    Initial Commit

---
+++ Panek/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Panek
+//
+//  Created by Piotr Suwała on 27/05/2019.
+//  Copyright © 2019 Panek Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
